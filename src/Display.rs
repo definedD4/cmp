3096a11335ede1f22188a03d15ee69e85cf241bb
definedd4 <definedd4@gmail.com>
2016-04-25 20:22:16 +0300

    GCM

---
+++ src/Display.rs
@@ -0,0 +1,29 @@
+use glium::glutin::Window;
+
+struct Display {
+	window: Window,
+}
+
+impl Display {
+	pub fn new() -> Display {
+		let window = glium::glutin::WindowBuilder::new().build_glium().unwrap();
+		Display{window}
+	}
+
+	pub fn run(mut self&) {
+		use glium::{DisplayBuild, Surface};
+    
+	    loop {
+	        let mut target = self.window.draw();
+	        target.clear_color(0.0, 0.0, 1.0, 1.0);
+	        target.finish().unwrap();
+
+	        for ev in self.window.wait_events() {
+	            match ev {
+	                glium::glutin::Event::Closed => return,
+	                _ => ()
+	            }
+	        }
+	    }
+	}
+}
