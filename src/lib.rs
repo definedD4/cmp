e1dddec173e96949d8bc78426344ff30b8fd6902
definedd4 <definedd4@gmail.com>
2016-04-25 19:59:59 +0300

    GCM

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,19 @@
 extern crate glium;
 
 pub fn test_lib(){
-	println!("Lib works!");
+    use glium::{DisplayBuild, Surface};
+    let display = glium::glutin::WindowBuilder::new().build_glium().unwrap();
+
+    loop {
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 1.0, 1.0);
+        target.finish().unwrap();
+
+        for ev in display.poll_events() {
+            match ev {
+                glium::glutin::Event::Closed => return,
+                _ => ()
+            }
+        }
+    }
 }
